c72a5d6ecad358c763aca4b68cb52419b556efc7
SPC David Wiliams II <davidwilliams1102@yahoo.com>
2019-10-29 14:25:39 -0500

    added customer veiw

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [key]
+        public  string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastNAme { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public DateTime PickUpDate { get; set; }
+        public double Balance { get; set; }
+        public double Monthlycharge { get; set; }
+        public bool PickupConfirmed { get; set; }
+        public  DateTime StartDate { get; set; }
+        public  DateTime EndDate { get; set; }
+
+    }
+
+}
